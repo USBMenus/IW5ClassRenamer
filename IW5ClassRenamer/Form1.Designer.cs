bdbf9dc430599df2f1f65456caae10d3075fd2f3
USBMenus <69381115+USBMenus@users.noreply.github.com>
2023-05-20 16:47:07 -0500

    Initial commit

---
+++ IW5ClassRenamer/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace IW5ClassRenamer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 39);
+            this.textBox1.MaxLength = 20;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(223, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Made by kikkin";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Class 1",
+            "Class 2",
+            "Class 3",
+            "Class 4",
+            "Class 5",
+            "Class 6",
+            "Class 7",
+            "Class 8",
+            "Class 9",
+            "Class 10",
+            "Class 11",
+            "Class 12",
+            "Class 13",
+            "Class 14",
+            "Class 15"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(223, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.Text = "Select A Class";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(247, 73);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "IW5 Class Renamer";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ IW5ClassRenamer/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using Memory;
+
+namespace IW5ClassRenamer
+{
+    public partial class Form1 : Form
+    {
+        Mem m = new Mem();
+        public string[] classes = 
+        {
+        "01CE53F8",
+        "01CE545A",
+        "01CE54BC",
+        "01CE551E",
+        "01CE5580",
+        "01CE55E2",
+        "01CE5644",
+        "01CE56A6",
+        "01CE5708",
+        "01CE576A",
+        "01CE57CC",
+        "01CE582E",
+        "01CE5890",
+        "01CE58F2",
+        "01CE5954" 
+        };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Process[] proc = Process.GetProcessesByName("plutonium-bootstrapper-win32");
+            if (proc.Length == 0) MessageBox.Show("Make Sure the Game Is Running", "Game Not Detected", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else if (proc.Length > 1) MessageBox.Show("Too Many Instances Open\nTry Closing The Others With Task Manager", "Too Many Instances", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else
+            {
+                m.OpenProcess(Process.GetProcessesByName("plutonium-bootstrapper-win32").FirstOrDefault().Id);
+                textBox1.Text = m.ReadString(classes[comboBox1.SelectedIndex], "", 20);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Process[] proc = Process.GetProcessesByName("plutonium-bootstrapper-win32");
+            if (proc.Length == 0) MessageBox.Show("Make Sure the Game Is Running", "Game Not Detected", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else if (proc.Length > 1) MessageBox.Show("Too Many Instances Open\nTry Closing The Others With Task Manager", "Too Many Instances", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else if (comboBox1.SelectedIndex == -1) MessageBox.Show("You Must Select A Class", "Select A Class", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+            {
+                m.OpenProcess(Process.GetProcessesByName("plutonium-bootstrapper-win32").FirstOrDefault().Id);
+                m.WriteMemory(classes[comboBox1.SelectedIndex], "bytes", "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0");
+                m.WriteMemory(classes[comboBox1.SelectedIndex], "string", textBox1.Text);
+            }
+        }
+    }
+}
